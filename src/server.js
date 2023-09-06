317b4e7648441954841fc93d99511b121c68a626
QuirogaAB <quiroga.ab@gmail.com>
2023-09-05 23:59:15 -0300

    modificacion en archivo server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
-import * as express from "express";
-import ProductManager from "./components.js/ProductManager";
+import   express from "express"
+import ProductManager from "./components.js/ProductManager.js";
 
 const app = express();
 app.use(express.urlencoded({extends : true}));
 
 
 const productos = new ProductManager()
+const readProducts = productos.readProducts()
+
+
+console.log(readProducts)
