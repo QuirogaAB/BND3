87917b402299a202412d456fda8cf009e735450b
QuirogaAB <quiroga.ab@gmail.com>
2023-09-12 22:23:56 -0300

    Modificacion en archivo server.js

--- src/server.js
+++ src/server.js
@@ -5,8 +5,21 @@ const app = express();
 app.use(express.urlencoded({extends : true}));
 
 
-const productos = new ProductManager()
-const readProducts = productos.readProducts()
+const productos = new ProductManager();
+async function main(){
+    await productos.addProduct("Pañales Dia","Pack de pañales para adulto absorventes de uso diario", 12000, "imagen1", "pa0355", 30);
+
+    await productos.addProduct( "Pañales Noche", "Pack de pañales para adulto ultraabsorventes de uso nocturno", 14000, "imagen2", "pa0356", 30 );
+
+    await productos.addProduct( "Jabon Hipoalergenico", "Jabon liquido de afrecho humectante", 3000, "imagen3", "ja0357", 10 );
+
+    await productos.addProduct( "Neceser", "Bolso Pequeño multiuso con cierre metalico", 2500, "imagen4", "ne0358", 15 );
+
+    await productos.getProducts();
+
+    await productos.getProductsById(2);
+    
+}
 
 
-console.log(readProducts)
+main();
